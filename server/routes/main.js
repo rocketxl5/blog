5a9f6bf83bc1c359ac616666299300f536c7dafe
Max Gaillard <maxgaillar@gmail.com>
2024-10-17 17:05:02 -0400

    Rough layout

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,7 +4,31 @@ const router = express.Router()
 
 // Routes
 router.get('', (req, res) => {
-    res.send("Hello")
+    const locals = {
+        title: "In Cognito Blog",
+        desc: "Simple Blog using NodeJs Express and MongoDB"
+    }
+    res.render('index', { locals })
 })
+
+/**
+ * GET /
+ * About
+*/
+router.get('/about', (req, res) => {
+    res.render('about', {
+        currentRoute: '/about'
+    });
+});
+
+/**
+ * GET /
+ * Contact
+*/
+router.get('/contact', (req, res) => {
+    res.render('contact', {
+        currentRoute: '/contact'
+    });
+});
 
 module.exports = router
